4f452e024af11c5d580ef22eed9351bd0ecf771c
support <support@kmcintosh-6782.remax.local>
2023-07-03 13:48:18 -0600

    created routes

--- db/migrate/20230703172444_create_reviews.rb
+++ db/migrate/20230703172444_create_reviews.rb
@@ -2,6 +2,10 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
 
+      t.integer :rating
+      t.string :comment
+      t.integer :user_id
+      t.integer :restaurant_id
       t.timestamps
     end
   end
--- db/migrate/20230703172557_create_restaurants.rb
+++ db/migrate/20230703172557_create_restaurants.rb
@@ -2,6 +2,9 @@ class CreateRestaurants < ActiveRecord::Migration[6.1]
   def change
     create_table :restaurants do |t|
 
+      t.string :name
+      t.string :address
+      t.string :description
       t.timestamps
     end
   end
